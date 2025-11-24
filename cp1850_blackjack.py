08b9b2e3699fb2fb6db5581672c009be3c749272
Bradley Charles Johnson <Bradley.Johnson@k202domain.csn.com>
2025-11-24 14:23:14 -0330

    changed list

--- cp1850_blackjack.py
+++ cp1850_blackjack.py
@@ -1,22 +1,19 @@
+import db
+
 PLAYER_MONEY = "money.txt"
 
-def read_money():
-    with open(PLAYER_MONEY, "r") as file:
-        money = file.read()
-        return money
-
-def write_money(money):
-    with open(PLAYER_MONEY, "w") as file:
-        file.write(money)
-
 def main():
-    suit = []
-    rank = []
-    pint_value = []
+    money = db.read_money(PLAYER_MONEY)
+
+    cards = [["Clubs","Diamonds","Hearts","Spades"],
+        ["Ace","Two","Three","Four","Five","Six","Seven",
+        "Eight","Nine","Ten","Jack","Queen","King"],
+        [1, 2, 3, 4, 5, 6,7, 8, 9, 10, 10, 10, 10]]
+
     dealers_hand = []
     players_hand = []
-    money = read_money()
-    print(money)
+    
+
 
 if __name__=="__main__":
     main()
---
+++ db.py
@@ -0,0 +1,12 @@
+def read_money(PLAYER_MONEY):
+    try:
+        with open(PLAYER_MONEY, "r") as file:
+            money = file.read()
+            return money
+    except FileNotFoundError:
+        print("Player money file not found!")
+
+
+def write_money(PLAYER_MONEY,money):
+    with open(PLAYER_MONEY, "w") as file:
+        file.write(money)
