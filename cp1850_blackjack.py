f553621f3daee0bf6bd8a927e9e3e373ac1114d2
brad <bradley.johnson05@ed.cna.nl.ca>
2025-12-01 21:22:40 -0330

    Deck randomization added

--- cp1850_blackjack.py
+++ cp1850_blackjack.py
@@ -7,17 +7,16 @@ def make_deck():
     ranks = ["Ace","Two","Three","Four","Five","Six","Seven",
         "Eight","Nine","Ten","Jack","Queen","King"]
     point_value = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] * 4
-
     deck = []
-
     for suit in suits:
         for rank,point in zip(ranks,point_value):
             deck.append(f"{rank} of {suit}")
+    random.shuffle(deck)
+    return deck
 
-    print(deck)
 def main():
     money = db.read_money(PLAYER_MONEY)
-    make_deck()
+    deck = make_deck()
 
     dealers_hand = []
     players_hand = []
